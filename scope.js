ddd10d024cad135192803c56ef8c3e0bdd10399b
BranLiang <lby89757@hotmail.com>
2016-09-28 15:19:08 +0800

    update

--- scope.js
+++ scope.js
@@ -1,26 +1,27 @@
 var ScopesApp = angular.module('ScopesApp', []);
 
-ScopesApp.controller('ScopesCtrl', ['$scope', function ($scope) {
+ScopesApp.controller('ScopesCtrl', ['$scope', '$window', function ($scope, $window) {
 	$scope.oneWay = "one way variable";
 	$scope.twoWay = "two way variable";
 
 	$scope.sayHello = function (name) {
-		console.log("Hello " + name);
+		$window.alert("Hello " + name);
 	};
 }]);
 
-ScopesApp.directive('isolated', function () {
+ScopesApp.directive('isolated', ['$window', function ($window) {
 	return {
 		templateUrl: "directive/isolated.html",
 		restrict: "E",
 		scope: {
 			twoWay: "=",
 			oneWay: "@",
-			sayHello: "&"
+			sayHello: "&",
 		},
 		link: function (scope) {
 			scope.twoWay = "two way changed";
 			scope.oneWay = "one way changed";
+			console.log(scope.sayHello);
 		},
 	};
-});
+}]);
